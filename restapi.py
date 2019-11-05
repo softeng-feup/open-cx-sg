7504d15912cfe3f5d1f1d0afbd40dbc8dc0ef351
joannatrojak <42413888+joannatrojak@users.noreply.github.com>
2019-11-05 19:06:22 +0000

    rest api added to the repository

---
+++ restapi.py
@@ -0,0 +1,23 @@
+import requests as rq 
+import json as js
+import re as r
+a = rq.get("https://2019.programming-conference.org/dataexport/810b23a0-737b-4f74-9170-75d515274859/confero.json")
+my_objects = js.loads(a.text)
+my_data = my_objects['Items']
+f = open("data.txt", "w")
+
+for item in my_data: 
+    itemKeys = item.keys()
+    replaceWithEmptyString = lambda string : string.replace("String", "").replace("u\'", "")
+    for key in itemKeys: 
+	if isinstance(item[key], list): 
+	     f.write(key)
+	     for element in item[key]: 
+                 f.write(element.encode("utf-8") + ",")
+        else: 
+             f.write(replaceWithEmptyString(key).encode("utf-8") + ": ")
+	     f.write("\n")
+             f.write(replaceWithEmptyString(item[key]).encode("utf-8"))
+	    
+
+f.close()
