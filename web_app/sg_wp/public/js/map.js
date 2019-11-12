f6b7165d8f0e3a6cc0d3589a96d3db6d4f2c5e63
joannatrojak <42413888+joannatrojak@users.noreply.github.com>
2019-11-12 12:29:04 +0000

    image showing correctly in the homepage

--- web_app/sg_wp/public/js/map.js
+++ web_app/sg_wp/public/js/map.js
@@ -2,6 +2,6 @@ var map = L.map('mapid', {
     crs: L.CRS.Simple
 }).setView([51.505, -0.09], 13);
 var bounds = [[0,0], [1000,1000]];
-var image = L.imageOverlay('open-cx-sg/web_app/sg_wp/public/js/map1.png', bounds).addTo(map);
+var image = L.imageOverlay('https://sigarra.up.pt/feup/pt/instal_geral2.get_mapa?pv_id=74657', bounds).addTo(map);
 map.fitBounds(bounds);
 console.log(image);
